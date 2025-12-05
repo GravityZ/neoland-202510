e3da947f880921e60ca958f428f59c8a1b3db6a1
GravityZ <gravityz231.ba231@gmail.com>
2025-12-05 20:59:25 +0100

    update

--- staff/ibra-ba/product/app/populate.js
+++ staff/ibra-ba/product/app/populate.js
@@ -1,2 +1,10 @@
 data.insertUser(new User('user-' + data.usersCount, 'Peter Pan', 'peterpan@gmail.com', 'peterpan', '12345678', 'regular'))
 data.insertUser(new User('user-' + data.usersCount, 'Wendy Darling', 'darlingwndy@gmail.com', 'wendddy', 'sisiwendysi', 'regular'))
+
+
+data.insertPet(new Pet('pet-' + data.petsCount, 'user-0', 'Sotano', '2024-12-01', 20, 'https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExMnFjeGpleGtkcjBvZjg2ZGVkbzZyMmxwNjZ6eDAxZmNpZ3k3ZmJtbSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/04DMYESomjb6BCBNB8/giphy.gif'))
+data.insertPet(new Pet('pet-' + data.petsCount, 'user-0', 'Tobogan', '2023-10-02', 4, 'https://media.tenor.com/cb9L14uH-YAAAAAM/cool-fun.gif'))
+data.insertPet(new Pet('pet-' + data.petsCount, 'user-0', 'Donut', '2020-10-02', 1, 'https://media.tenor.com/1bbw0tUTEpYAAAAM/turtle-slippery.gif'))
+
+
+data.insertPet(new Pet('pet-' + data.petsCount, 'user-1', 3,'https://media.tenor.com/_WZy7E7hoTcAAAAM/cat-smile.gif'))
